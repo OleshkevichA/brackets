43b653445db862f637e9760d26744b4fce826f41
OleshkevichA <oleshkevichA85@gmail.com>
2022-02-15 10:00:26 +0300

    I did it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = str;
+  let b = bracketsConfig;
+  let result = b.map( elem => elem.reduce( (q, w) => q + w));
+
+  for (let i = 0; i < str.length; i++){
+    result.map( elem => a = a.replace(elem, ''));
+ }
+  if (a.length == 0){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
